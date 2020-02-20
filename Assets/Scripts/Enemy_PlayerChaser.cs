07c8e56fdc18781d4a1efde94f3c9cd5627a28f7
Bozodo <aquinojt.1@gmail.com>
2020-02-20 16:32:21 -0500

    Created an enemy that lunges at the player.

---
+++ Assets/Scripts/Enemy_PlayerChaser.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_PlayerChaser : MonoBehaviour
+{
+    public GameObject player;
+    public Transform playerTransform;
+    public int MoveSpeed = 1; // you can adjust speed based on different level
+    public int trackingDistance = 0;
+    public int lungeDistance = 3;
+    public int lungeMultiplier = 3;
+    public int waitTime = 3;
+    private Rigidbody enemyRB;
+
+    [Header("Dynamic Stuff")]
+    public Vector3 enemyPos;
+    public float distanceFromPlayer;
+    public Vector3 enemyVelocity;
+    public bool canMove = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRB = GetComponent<Rigidbody>();
+        StartCoroutine(waiter());
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        enemyPos = transform.position;
+        enemyVelocity = enemyRB.velocity;
+        distanceFromPlayer = Vector3.Distance(enemyPos, playerTransform.position);
+        
+
+        //transform.LookAt(playerTransform);
+
+        if(canMove == true)
+        {
+            enemyRB.AddForce((playerTransform.position - transform.position) * MoveSpeed);
+
+            if (distanceFromPlayer <= lungeDistance)
+            {
+                enemyRB.AddForce((playerTransform.position - transform.position) * MoveSpeed * lungeMultiplier);
+            }
+        }
+        
+
+        
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject collidedWith = collision.gameObject;
+        if (collidedWith.tag == "Player")
+        {
+            //yield return new WaitForSecondsRealtime(5);
+            enemyRB.Sleep();
+            StartCoroutine(waiter());
+        }
+    }
+
+    IEnumerator waiter()
+    {
+        canMove = false;
+
+        yield return new WaitForSecondsRealtime(waitTime);
+
+        canMove = true;
+    }
+}
